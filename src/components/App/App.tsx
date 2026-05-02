b1e11f9f85e98ba9f50638ec8283b1c67931799e
Tanya Tychynska <tatyanazyeva@gmail.com>
2026-05-02 19:32:21 +0200

    getPhotos

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,31 @@
 import Section from "../Section/Section";
 import Container from "../Container/Container";
 import Form from "../Form/Form";
+import Text from "../Text/Text";
 import { Toaster } from "react-hot-toast";
 import { getPhotos } from "../../services/photos";
+import { useState } from "react";
+import type { Photo } from "../../types/photo";
+import Loader from "../Loader/Loader";
 
 export default function App() {
-  const handleSearch = (value: string) => {
-    console.log(value);
-    getPhotos(value);
+  
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSearch = async (value: string) => {
+    try { 
+      setIsError(false);
+      setIsLoading(true);
+      const res = await getPhotos(value);
+      setPhotos(res);
+    }
+    catch {
+      setIsError(true);
+    }
+    finally {
+      setIsLoading(false);
+    }
   };
 
   return (
@@ -16,9 +34,12 @@ export default function App() {
         <Container>
           <Form onSubmit={handleSearch} />
           <Toaster />
+          {isError && <Text>Something went wrong</Text>}
+          {isLoading && <Loader/>}
         </Container>
         Home page
       </Section>
+      
     </>
   );
 }
